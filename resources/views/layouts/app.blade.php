554ef02ef2bde91723e6ea87e1415e5907e256c9
Jenderal Software <jenderal.software@gmail.com>
2025-11-28 21:06:28 +0700

    add authentication middleware to all routes and hide navigation menu for guests

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,8 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                   
+                    @auth
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
@@ -40,6 +42,7 @@
                             <a class="nav-link" href="{{ route('berita.index') }}">Berita</a>
                         </li>
                     </ul>
+                    @endauth
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
--- routes/web.php
+++ routes/web.php
@@ -9,23 +9,22 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('jenis-berita', [App\Http\Controllers\JenisBeritaController::class, 'index'])->name('jenis-berita.index');
-Route::get('jenis-berita/create', [App\Http\Controllers\JenisBeritaController::class, 'create'])->name('jenis-berita.create');
-Route::post('jenis-berita', [App\Http\Controllers\JenisBeritaController::class, 'store'])->name('jenis-berita.store');
-Route::get('jenis-berita/{jenisBerita}', [App\Http\Controllers\JenisBeritaController::class, 'show'])->name('jenis-berita.show');
-Route::get('jenis-berita/{jenisBerita}/edit', [App\Http\Controllers\JenisBeritaController::class, 'edit'])->name('jenis-berita.edit');
-Route::put('jenis-berita/{jenisBerita}', [App\Http\Controllers\JenisBeritaController::class, 'update'])->name('jenis-berita.update');
-Route::delete('jenis-berita/{jenisBerita}', [App\Http\Controllers\JenisBeritaController::class, 'destroy'])->name('jenis-berita.destroy');
+    Route::get('jenis-berita', [App\Http\Controllers\JenisBeritaController::class, 'index'])->name('jenis-berita.index');
+    Route::get('jenis-berita/create', [App\Http\Controllers\JenisBeritaController::class, 'create'])->name('jenis-berita.create');
+    Route::post('jenis-berita', [App\Http\Controllers\JenisBeritaController::class, 'store'])->name('jenis-berita.store');
+    Route::get('jenis-berita/{jenisBerita}', [App\Http\Controllers\JenisBeritaController::class, 'show'])->name('jenis-berita.show');
+    Route::get('jenis-berita/{jenisBerita}/edit', [App\Http\Controllers\JenisBeritaController::class, 'edit'])->name('jenis-berita.edit');
+    Route::put('jenis-berita/{jenisBerita}', [App\Http\Controllers\JenisBeritaController::class, 'update'])->name('jenis-berita.update');
+    Route::delete('jenis-berita/{jenisBerita}', [App\Http\Controllers\JenisBeritaController::class, 'destroy'])->name('jenis-berita.destroy');
 
-Route::get('berita', [App\Http\Controllers\BeritaController::class, 'index'])->name('berita.index');
-Route::get('berita/create', [App\Http\Controllers\BeritaController::class, 'create'])->name('berita.create');
-Route::post('berita', [App\Http\Controllers\BeritaController::class, 'store'])->name('berita.store');
-Route::get('berita/{berita}', [App\Http\Controllers\BeritaController::class, 'show'])->name('berita.show');
-Route::get('berita/{berita}/edit', [App\Http\Controllers\BeritaController::class, 'edit'])->name('berita.edit');
-Route::put('berita/{berita}', [App\Http\Controllers\BeritaController::class, 'update'])->name('berita.update');
-Route::delete('berita/{berita}', [App\Http\Controllers\BeritaController::class, 'destroy'])->name('berita.destroy');
-
-
-
+    Route::get('berita', [App\Http\Controllers\BeritaController::class, 'index'])->name('berita.index');
+    Route::get('berita/create', [App\Http\Controllers\BeritaController::class, 'create'])->name('berita.create');
+    Route::post('berita', [App\Http\Controllers\BeritaController::class, 'store'])->name('berita.store');
+    Route::get('berita/{berita}', [App\Http\Controllers\BeritaController::class, 'show'])->name('berita.show');
+    Route::get('berita/{berita}/edit', [App\Http\Controllers\BeritaController::class, 'edit'])->name('berita.edit');
+    Route::put('berita/{berita}', [App\Http\Controllers\BeritaController::class, 'update'])->name('berita.update');
+    Route::delete('berita/{berita}', [App\Http\Controllers\BeritaController::class, 'destroy'])->name('berita.destroy');
+});
